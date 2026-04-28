e0e7716bc08f0d3ef4f3d44de8daa0a8092eed72
AmmanRizwan <ammanrizwan00007@gmail.com>
2026-04-28 19:05:39 +0530

    fix: replace fetch with api client for token retrieval and flight data fetching

--- backend/src/service/flight/index.ts
+++ backend/src/service/flight/index.ts
@@ -1,3 +1,4 @@
+import api from "../api";
 import env from "../../config/env";
 
 const getFlights = async () => {
@@ -6,43 +7,25 @@ const getFlights = async () => {
     params.append('client_id', env.FLIGHT.CLIENT.ID);
     params.append('client_secret', env.FLIGHT.CLIENT.SECRET);
 
-    try {
-        const token_data = await fetch(env.FLIGHT.TOKEN_API as string, {
-            method: "POST",
+    const token_data = await api.post(env.FLIGHT.TOKEN_API as string, params, {
+        headers: {
+            "Content-Type" : "application/x-www-form-urlencoded"
+        },
+        family: 4,
+    });
+
+    if (token_data) {
+        const token = token_data.data.access_token;
+        const response = await api.get("/all", {
             headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Accept': 'application/json',
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-                'Accept-Encoding': 'gzip'
-            },
-            body: params
-        })
-
-        if (!token_data.ok) {
-            throw new Error(`'HTTPS error' status: ${token_data.status}`);
-        }
-
-        const data = await token_data.json();
-
-        if (data) {
-            const token = data.access_token; 
-            const response = await fetch(`${env.FLIGHT.API}/all`
-                , {
-                headers: {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-                    "Authorization": `Bearer ${token}`,
-                    "Accept": 'application/json'
-                }
+                "Authorization": `Bearer ${token}`,
+                "Accept": 'application/json'
             }
-        );
-            if (!response.ok) {
-                throw new Error(`HTTPS error status: ${response.status}` );
-            }
-            return response.json();
-        }
-    }
-    catch (err) {
-        console.error("fetching error:", err);
+        });
+        return response.data;
+    } else {
+        const response = await api.get("/all");
+        return response.data;
     }
 }
 

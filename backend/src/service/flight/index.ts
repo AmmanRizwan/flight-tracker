ac47c87bb04e0f70ca7125d1555e359cdfea78af
AmmanRizwan <ammanrizwan00007@gmail.com>
2026-04-29 11:11:23 +0530

    fix: uncomment and restore flight data fetching logic in getFlights function

--- backend/src/service/flight/index.ts
+++ backend/src/service/flight/index.ts
@@ -2,34 +2,28 @@ import api from "../api";
 import env from "../../config/env";
 
 const getFlights = async () => {
-    // const params = new URLSearchParams();
-    // params.append('grant_type', env.FLIGHT.CLIENT.GRANT_TYPE);
-    // params.append('client_id', env.FLIGHT.CLIENT.ID);
-    // params.append('client_secret', env.FLIGHT.CLIENT.SECRET);
+    const params = new URLSearchParams();
+    params.append('grant_type', env.FLIGHT.CLIENT.GRANT_TYPE);
+    params.append('client_id', env.FLIGHT.CLIENT.ID);
+    params.append('client_secret', env.FLIGHT.CLIENT.SECRET);
 
-    // const token_data = await api.post(env.FLIGHT.TOKEN_API as string, params, {
-    //     headers: {
-    //         "Content-Type" : "application/x-www-form-urlencoded"
-    //     },
-    //     family: 4,
-    // });
+    const token_data = await api.post(env.FLIGHT.TOKEN_API as string, params, {
+        headers: {
+            "Content-Type" : "application/x-www-form-urlencoded"
+        },
+        family: 4,
+    });
 
-    // if (token_data) {
-    //     const token = token_data.data.access_token;
-    //     const response = await api.get("/all", {
-    //         headers: {
-    //             "Authorization": `Bearer ${token}`,
-    //             "Accept": 'application/json'
-    //         }
-    //     });
-    //     return response.data;
-    // } else {
-        const auth = Buffer.from(`${env.FLIGHT.USER}:${env.FLIGHT.PASS}`).toString('base64');
+    if (token_data) {
+        const token = token_data.data.access_token;
         const response = await api.get("/all", {
-            headers: { "Authorization": `Basic ${auth}` }
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Accept": 'application/json'
+            }
         });
         return response.data;
-    // }
+    }
 }
 
 export { 
